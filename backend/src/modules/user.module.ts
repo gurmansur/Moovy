81b302e5ea7ecb32ae3d1eb12f41dc64f386c162
Gustavo Mansur <gurmansur@hotmail.com>
2024-03-28 11:38:51 -0300

    Changed folder structure

--- backend/src/modules/user.module.ts
+++ backend/src/modules/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
-import { DatabaseModule } from "../database/database.module";
-import { userProviders } from "./user.providers";
-import { UserService } from "./user.service";
+import { DatabaseModule } from "./database.module";
+import { userProviders } from "../providers/user.providers";
+import { UserService } from "../services/user.service";
 
 @Module({
   imports: [DatabaseModule],
