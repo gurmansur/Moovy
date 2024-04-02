79342270fc0dd1e7521da4af6743b312b331fe9b
Gustavo Mansur <gurmansur@hotmail.com>
2024-04-02 18:06:55 -0300

    Fixed My Library layout

--- frontend/src/pages/MyLibrary.js
+++ frontend/src/pages/MyLibrary.js
@@ -35,9 +35,9 @@ const MyLibrary = () => {
     return(
         <div>
             <Typography variant="h5" color="initial">My Library</Typography>
-            <Grid container spacing={2} justifyContent="center" direction="row" sx={{ pl: "15vw", pr: "15vw", pt: "5vh" }}>
+            <Grid container spacing={2} justifyContent="center" direction="row" sx={{ pl: 15, pr: 15, pt: 5 }}>
                     {movies.map(movie => (
-                        <Grid item xs={4} key={movie.imdbId}>
+                        <Grid item key={movie.imdbId}>
                             <MovieCard movie={movie} />
                         </Grid>
                     ))}
