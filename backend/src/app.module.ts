9139c705a803ff3191b7f3863481f16e90262dd2
Gustavo Mansur <gurmansur@hotmail.com>
2024-04-03 09:44:47 -0300

    Added .env support to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,15 +6,16 @@ import { LibraryEntry } from './typeorm/entities/LibraryEntry';
 import { LibraryEntryModule } from './library-entry/library-entry.module';
 import { AuthModule } from './auth/auth.module';
 import { MoviesModule } from './movies/movies.module';
+require('dotenv').config();
 
 @Module({
   imports: [UsersModule, LibraryEntryModule, TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'admin',
-    database: 'moovy',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [User, LibraryEntry],
     synchronize: true
   }), AuthModule, MoviesModule],
